bffac91bac8476988518e3c04184e47843f8ba5c
Sana <sbh@Sanas-MacBook-Pro.local>
2024-10-29 11:33:56 +0200

    Add  main's pseudocode

---
+++ WeDeliver/main.py
@@ -0,0 +1,16 @@
+*Pseudocode*
+Start:
+    DISPLAY "Hello!Please enter":
+    1."Go to Driver's Menu"
+    2."Go to Cities' Menu"
+    3."Exit"
+
+    INPUT
+       IF INPUT = 1:
+            CALL DriversMenu()
+        IF INPUT = 2:
+            CALL CitiesMenu()
+        IF INPUT = 3:
+            EXIT
+
+END
