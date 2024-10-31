89fe8ac579f312533ca13db3ffe857be9477f291
Sana <sbh@Sanas-MacBook-Pro.local>
2024-10-31 13:30:32 +0200

    Implementation of drivers' functions code

---
+++ WeDeliver/driversfunction.1.py
@@ -0,0 +1,116 @@
+
+'''drivers = {"start_city": [{"ID": "", "name": ""}]}
+cities = []
+
+
+def view_all_drivers(drivers):
+    for city, driver_list in drivers:
+        for driver in drivers_list:
+            DISPLAY  "ID:", driver, ID "Name:", driver.name, "Start City:" driver.start_city
+        END for
+    END for
+
+
+END def view_all_drivers():
+
+
+def add_driver(drivers, cities):
+    Prompt: "Enter driver's name:"
+    INPUT name
+    Prompt: "Enter start city:"
+    INPUT start_city
+
+    if start_city in cities:
+        GENERATE Id for new driver
+
+        if start_city not in drivers:
+            drivers[start_city] = []
+        ADD(ID, name, start_city) to drivers[start_city] = []
+
+    else:
+        Prompt: "The city is not available, add it? (yes/no)"
+        INPUT answer
+        if answer == "yes":
+            ADD start_city to cities
+            CALL add_driver(drivers, cities)
+        else:
+            RETURN to drivers_menu()
+
+        END if
+    END if
+
+
+END def add_driver()
+
+
+def check_similar_drivers(drivers):
+    for city, driver_list in drivers:
+        DISPLAY city: ADD(driver.name for driver in drivers_list)"
+    END for
+    DISPLAY "Return to Main Menu? (yes to return)"
+    INPUT
+    if INPUT = "yes":
+        CALL main()
+    END if
+END def check_similar_drivers()'''
+
+
+drivers = {
+    "Akkar": [{"ID": "ID001", "name": "Max Verstappen"}],
+    "Saida": [{"ID": "ID002", "name": "Charles Leclerc"}],
+    "Jbeil": [{"ID": "ID003", "name": "Lando Norris"}]
+}
+cities = ["Beirut", "Jbeil", "Akkar", "Zahle", "Saida"]
+
+
+def view_all_drivers(drivers):
+    for start_city, driver_list in drivers.items():
+        for driver in driver_list:
+            print(f'ID: {driver["ID"]},Name: {
+                  driver["name"]}, Start City: {start_city}')
+    input("Press enter to go back to main menu")
+    drivers_menu()
+
+
+def add_driver(drivers, cities):
+    name = input("Enter driver's name: ")
+    start_city = input("Enter start city: ")
+
+    if start_city in cities:
+        id = max(
+            (int(driver["ID"][2:]) for start_city,
+             driver_list in drivers.items() for driver in driver_list),
+            default=0
+        )
+        id_new = f'ID{id+1:03}'
+        driver_new = {"ID": id_new, "name": name}
+
+        if start_city not in drivers:
+            drivers[start_city] = []
+
+        drivers[start_city].append(driver_new)
+        print(f"{driver_new} added successfully")
+
+    else:
+        answer = input("The city is not available, add it? (yes/no)")
+
+        if answer.lower() == "yes":
+            cities.append(start_city)
+            add_driver(drivers, cities)
+        else:
+            print("Driver was not added.")
+
+    drivers_menu()
+
+
+def check_similar_drivers(drivers):
+    for city, driver_list in drivers.items():
+        print(city + ":" + ",".join(driver["name"]for driver in driver_list))
+
+    answer = input("Return to Main Menu? (yes to return)")
+
+    if answer.lower() == "yes":
+        main()
+
+
+main()
