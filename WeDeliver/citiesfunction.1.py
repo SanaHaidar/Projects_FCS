0c775bd961b087b2c1e7671c1b457c7196df2deb
Sana <sbh@Sanas-MacBook-Pro.local>
2024-11-03 18:15:03 +0200

    Implementaticode  cities function

--- WeDeliver/citiesfunction.1.py
+++ WeDeliver/citiesfunction.1.py
@@ -40,7 +40,12 @@ def dirvers_delivering():
 
 
 END def dirvers_delivering():'''
-
+from collections import deque
+drivers = {
+    "Akkar": [{"ID": "ID001", "name": "Max Verstappen"}],
+    "Saida": [{"ID": "ID002", "name": "Charles Leclerc"}],
+    "Jbeil": [{"ID": "ID003", "name": "Lando Norris"}]
+}
 
 cities = {
     "Beirut": ["Jbeil"],
@@ -58,8 +63,8 @@ def show_cities():
 
 
 def search_cities():
-    key = input("Enter search key :").strip()
-    cities_key = [city for city in cities.keys()if key in city]
+    key = input("Enter search key :").strip().lower()
+    cities_key = [city for city in cities if key in city]
     if cities_key:
         print(f"Cities containing '{key}': {','.join(cities_key)}")
     else:
@@ -67,7 +72,7 @@ def search_cities():
 
 
 def neighboring_cities():
-    city = input("Enter city name").strip()
+    city = input("Enter city name").strip().lower()
     if city in cities:
         neighboring_cities = cities[city]
         if neighboring_cities:
@@ -77,3 +82,37 @@ def neighboring_cities():
             print(f"No Neighboring cities to '{city}'.")
     else:
         print("City not registered")
+
+
+def bfs(start_city, visited, drivers_delivering):
+    queue = deque([start_city])
+    visited.add(start_city)
+
+    while queue:
+        current = queue.popleft()
+
+        if current in drivers:
+            for driver in drivers[current]:
+                drivers_delivering.add(driver["name"])
+
+        for neighbor in cities.get(current, []):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+
+def drivers_delivering():
+
+    city = input("Enter city name: ").strip().lower()
+    visited = set()
+    drivers_delivering = set()
+
+    if city in cities:
+        bfs(city, visited, drivers_delivering)
+        if drivers_delivering:
+            print(f"\n Driversdelivering to {city}:{
+                  ','.join(drivers_delivering)}")
+        else:
+            print(f'No drivers found delivering to {city.capitalize()}')
+    else:
+        print("City not registered.")
